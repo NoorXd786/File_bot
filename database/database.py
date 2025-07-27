830f200064b2f847d4f202c1c767372c9e46e8f6
Md Noor <75603153+NoorXd786@users.noreply.github.com>
2025-07-27 20:27:03 +0530

    Update database.py

--- database/database.py
+++ database/database.py
@@ -1,11 +1,21 @@
 #(©)CodeXBotz
 
 import pymongo, os
+from pymongo import MongoClient
+
+DB_URI = os.getenv("DB_URI")
+
+# Temporary Debug — Add this
+if not DB_URI:
+    raise ValueError("DB_URI is empty or not set!")
+
+dbclient = MongoClient(DB_URI)
 from config import DB_URI, DB_NAME
 
 dbclient = pymongo.MongoClient(DB_URI)
 database = dbclient[DB_NAME]
 user_data = database['users']
+
 
 async def present_user(user_id : int):
     found = user_data.find_one({'_id': user_id})
